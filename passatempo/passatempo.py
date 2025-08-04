8b15c9ec7f7a8d7155e427f3b0b1f1dc05b7c9e6
José Satiro <jardelsatiro57@gmail.com>
2025-08-03 21:20:41 -0300

    Update passatempo.py

--- passatempo/passatempo.py
+++ passatempo/passatempo.py
@@ -5,6 +5,8 @@ variaveis = {}
 lista_var = []
 xs = []
 S = []
+variaveis_achadas = []
+
 if 1 <= l and l <= 100:
     for linha in range(l+1):
         if linha == l:
@@ -25,59 +27,123 @@ if 1 <= l and l <= 100:
             sub_var = []
             for indice in range(len(var)-1):
                 if var[indice] not in variaveis:
-                    variaveis[var[indice]] = 0
+                    variaveis[var[indice]] = '0'
                 sub_var.append(var[indice])
             lista_var.append(sub_var)
             
 lista_var_copia = lista_var
 
 # print(variaveis)
-cont1 = 0
-volta = 0
-for i in range(len(lista_var_copia)):
-    i_certo = i
-    cont2 = 0
-    verificacao = True
-    indiceLinha = 's'
-    indiceColuna = 's'
-    nome_var_linha = ''
-    while cont2 < len(lista_var_copia):
-        if volta >= 1:
-            i_certo = i - 1
-            print('z')
-        if lista_var_copia[i_certo][cont2] == lista_var_copia[i_certo][cont2+1]:
-            cont2 +=1
-            indiceLinha = cont1 - volta
-            nome_var_linha = lista_var_copia[i_certo][cont2-volta]
-        else:
-            indiceLinha = -1
-            verificacao = False
-            break
-    cont2 = 0
-    nome_var_coluna = ''
-    while cont2 < len(lista_var_copia):
-        if lista_var_copia[i_certo][cont2] == lista_var_copia[i_certo][cont2+1]:
-            cont2 +=1
-            indiceLinha = cont1
-        else:
-            indiceLinha = -1
-            verificacao = False
-            break      
-    print(nome_var_linha)
-    if verificacao and indiceLinha != 's':
-        print(indiceLinha)
-        valor_var = S[indiceLinha] / len(lista_var_copia[indiceLinha])
-        S[indiceLinha] = S[indiceLinha] - valor_var
-        variaveis[nome_var_linha] = valor_var
-        lista_var_copia.pop(indiceLinha)
-    elif verificacao and indiceColuna != 's':
-        pass
-    print()
-    print(lista_var_copia)
-    print(S)
-    print(variaveis)
+# while len(lista_var_copia) > 0:
+def dar_bom_por_favor():
+    voltaC = 0
+    cont1C = 0
+    cont2C = 0
+    contColunaC = 0
 
-    cont1 += 1
-    volta += 1
+    for i in range(len(lista_var_copia)):
 
+
+        verificacaoLinha = True
+        indiceLinha = 's'
+        indiceColuna = 's'
+        nome_var_linha = ''
+        
+        while cont2C < len(lista_var_copia[0])-1:
+
+            if len(lista_var_copia[0]) != 1 and len(lista_var_copia[0]) != 0:
+
+                
+                if lista_var_copia[i][cont2C] == lista_var_copia[i][cont2C+1]:
+
+                    
+                    indiceLinha = i
+
+
+                    nome_var_linha = lista_var_copia[i][cont2C]
+                    
+                else:
+                    indiceLinha = -1
+                    verificacaoLinha = False
+                    break
+            else:
+                verificacaoLinha = False
+            cont2C += 1
+            
+        nome_var_coluna = ''
+        
+        # while contColunaC < len(lista_var_copia):
+        #     linhas = len(lista_var_copia)
+        #     colunas = len(lista_var_copia[0])
+        verificacaoColuna = True
+        for lin in range(len(lista_var_copia)-1):
+            if contColunaC < len(lista_var_copia[0]):
+                if lista_var_copia[lin][contColunaC] == lista_var_copia[lin+1][contColunaC]:
+                    indiceColuna = contColunaC
+
+                    nome_var_coluna = lista_var_copia[lin][contColunaC]
+                else:
+                    contColunaC += 1  
+                    verificacaoColuna = False
+                    break  
+        
+
+        if verificacaoLinha == True and indiceLinha != 's':
+            print(verificacaoLinha)
+            valor_var = S[cont1C] / len(lista_var_copia[i])
+            S.remove(S[cont1C])
+            xs[cont1C] -= valor_var
+            variaveis[nome_var_linha] = valor_var
+            lista_var_copia.pop(indiceLinha)
+            variaveis_achadas.append(nome_var_linha)
+            # for ind in range(len(lista_var_copia[0])):
+            #     for lista in lista_var_copia:
+
+            #         if lista[ind] == nome_var_linha:
+            #             S[ind] -= valor_var
+            #             xs[ind] -= valor_var
+        elif verificacaoColuna and indiceColuna != 's':
+
+            valor_var = xs[indiceColuna] / len(lista_var_copia)
+            S[indiceColuna] -= valor_var
+            variaveis[nome_var_coluna] = valor_var
+            variaveis_achadas.append(nome_var_coluna)
+
+            for linha in range(len(lista_var_copia)):
+                lista_var_copia[linha].pop(indiceColuna)
+                if linha == len(lista_var_copia)-1:
+                    xs.remove(xs[indiceColuna])
+            # for ind in range(len(lista_var_copia[0])):
+            #     for lista in lista_var_copia:
+
+            #         if lista[ind] == nome_var_coluna:
+            #             S[ind] -= valor_var
+            #             xs[ind] -= valor_var
+        voltaC += 1
+        for index in range(len(lista_var_copia)):
+            conta = 0
+            for lista in range(len(lista_var_copia[index][0])):
+                print(lista_var_copia)
+                print()
+                print(conta)
+                print(lista)
+                print(lista_var_copia[lista][conta])
+                if lista_var_copia[lista][conta] in variaveis_achadas:
+                    lista_var_copia.remove(lista_var_copia[lista][conta])
+                conta += 1
+
+    if len(lista_var_copia) == 1:
+        return 'deu certo'
+    else:
+        for lista in lista_var_copia:
+            print(lista)
+        print()
+        return dar_bom_por_favor()
+ind = 0
+
+dar_bom_por_favor()
+
+print(S)
 print(xs)
+for k, v in variaveis.items():
+    print(k, v)
